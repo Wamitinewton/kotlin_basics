7febd0d116aa57bdef1175e1927c0cfc4bd6a6d4
wamitinewton <wamitinewton@gmail.com>
2024-05-16 16:09:08 +0300

    push

---
+++ variable_data_types.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>){
+    var myNumber = 10;
+    var myDecimal = 10.5;
+    var isActive = true;
+    var myString: String
+    // mutable string as it can change the state over time
+    myString = "Hello word"
+    myString = "Another world"
+
+    // immutable string as it cannot change over time and if changed it will throw an error
+    val myAnotherString = "My constant string is here"
+//    myAnotherString = "My constant string is changed"
+
+    print(myNumber)
+
+
+}
